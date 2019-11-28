b070784498c0a08d804d48b4eaf6cb3532ba0b05
lazzaro <734701641@qq.com>
2019-11-28 14:24:37 +0800

    新增redis

---
+++ src/main/Test/JedisDome1.java
@@ -0,0 +1,21 @@
+import com.jwh.MpGaojiApplication;
+import com.jwh.utils.RedisUtil;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = MpGaojiApplication.class)
+public class JedisDome1 {
+
+    @Autowired(required=true)
+    private RedisUtil redisUtil;
+
+    @Test
+    public void Test1(){
+        System.out.println(redisUtil.get("a1"));
+    }
+}
